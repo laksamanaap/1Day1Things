4cc8da88a37655c19c44e21ec9eba790c0fae058
Laksamana <103801675+laksamanaap@users.noreply.github.com>
2024-07-29 22:46:00 +0700

    Create Maskify.ts

---
+++ Maskify.ts
@@ -0,0 +1,11 @@
+// Change all string char to "#" except the last 4 char elements
+export function maskify(cc: string): string {
+    if (cc.length <= 4) {
+        return cc;
+    }
+    const maskedPart = cc.slice(0, -4).replace(/./g, '#');
+    const visiblePart = cc.slice(-4);
+    return maskedPart + visiblePart;
+}
+
+console.log(maskify(4556364607935616)) // ############5616
