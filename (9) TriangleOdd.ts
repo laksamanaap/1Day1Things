2da800edadcee3f02e48a16c71839d25475d21fb
Laksamana <103801675+laksamanaap@users.noreply.github.com>
2024-08-01 21:22:38 +0700

    Update (9) TriangleOdd.ts

--- (9) TriangleOdd.ts
+++ (9) TriangleOdd.ts
@@ -1,14 +1,19 @@
 {/* 
-             1
-          3     5
-       7     9    11
-   13    15    17    19
-21    23    25    27    29
+
+             1                  = 1 (1³)
+          3     5               = 8 (2³)
+       7     9    11            = 27 (3³)
+   13    15    17    19         = 64 (4³)
+21    23    25    27    29      = 125 (5³)
     
 Calculate the number base on their rows
+
+If we sum all the odd number, we can look that the triangle pattern is (n^3)
+
 */}
 
-export function rowSumOddNumbers(n: number): number  {
-    return 0
+
+export function rowSumOddNumbers(n: number): number {
+    return n * n * n;
 }
 
